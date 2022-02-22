b07647f19aa8bebb218d443897517e0ea9f2570b
Patrick Welter <pa.welter@web.de>
2022-02-22 14:44:32 +0100

    Init backend

---
+++ backend/src/main/kotlin/com/wiuwiu1/financalplaner/FinancialPlanerApplication.kt
@@ -0,0 +1,11 @@
+package com.wiuwiu1.financalplaner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinancialPlanerApplication
+
+fun main(args: Array<String>) {
+    runApplication<FinancialPlanerApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/wiuwiu1/financalplaner/FinancialPlanerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wiuwiu1.financalplaner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinancialPlanerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
